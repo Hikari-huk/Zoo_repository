803a3468f2e569cfb91b42ff1ba8b96c633daf28
Touma-M08 <touma_064810@yahoo.co.jp>
2022-07-10 12:14:46 +0900

    トップページのviewファイルの完成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,43 +4,45 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css/top.css')}}">
     </head>
     <body>
-        <h1>レバテックチーム開発</h1>
-        <a href="/logout">logout</a>
-        <h2>投稿一覧ページ</h2>
-        <div class='posts'>
-            @foreach($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <p class='body'>本文：{{ $post->body}}</p>
-                    <p>カテゴリー:<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}"  method="post" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onClick="deletePost({{$post->id}});">削除</button> {{--script内に定義したdeletePostを使用している--}}
-                    </form>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-        <div>
-            [<a href='/posts/create'>新規作成</a>]
+        @extends("layouts/app")
+        @section("content")
+        <div class="wrapper">
+            <div class='posts'>
+                @foreach($posts as $post)
+                    <div class='post'>
+                        <a href="/show/{{ $post->id }}">
+                            <img src="{{$post->images_url}}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+            
+            <div class="create">
+                <a href='/create'>+</a>
+            </div>
+            
+            <nav class="footer-nav">
+                <ul class="nav-list">
+                    <li>
+                        <a class="toppage" href="/">トップページ</a>
+                    </li>
+                    <li>
+                        <a class="mypage" href="/mypage">マイページ</a>  {{--<a href="/mypage/{{$user->id}}">--}}
+                    </li>
+                    <li>
+                        <a class="search" href="/search">検索</a>
+                    </li>
+                    <li>
+                        <a class="ranking" href="/ranking">ランキング</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </body>
-    <script>
-        function deletePost(post_id) {
-            form = document.getElementById('form_' + post_id);  //各投稿ごとのdeleteのformを取得
-            is_submit = confirm('本当に削除してもよろしいですか？'); //はいの場合true,いいえの場合falseをis_submitに格納
-            
-            if(is_submit) {  //is_submitがtrueの場合のみ、{}の中の処理が行われる
-                form.submit();  //deleteするformをsubmitする（投稿を削除している）
-            }
-        }
-    </script>
+    @endsection
 </html>
